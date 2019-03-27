7fbabaef7cfdfd024dbc5d41e231e1dd91819afd
Jose Nidhin <jose.nidhin@transferto.com>
2019-03-27 18:08:10 +0800

    Add basic routing mechanism

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/josnidhin/go-rest-api-example
 
 go 1.12
+
+require github.com/gorilla/mux v1.7.0
--- main.go
+++ main.go
@@ -6,12 +6,39 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/josnidhin/go-rest-api-example/config"
 )
 
+type App struct {
+	Router *mux.Router
+}
+
 func main() {
 	config := config.Load()
+
 	configJson, _ := json.Marshal(config)
 	fmt.Println(string(configJson))
+
+	app := &App{}
+	app.Init(config)
+}
+
+func (app *App)Init(config *config.Config) {
+	app.Router = mux.NewRouter()
+
+	app.Get("/", func(w http.ResponseWriter, r *http.Request){
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("hello"))
+	})
+}
+
+func (app *App)Get(path string, fn func(w http.ResponseWriter, r *http.Request)) {
+	app.Router.HandleFunc(path, fn).Methods(http.MethodGet)
+}
+
+func (app *App)Post(path string, fn func(w http.ResponseWriter, r *http.Request)) {
+	app.Router.HandleFunc(path, fn).Methods(http.MethodPost)
 }
