c1a05a179c1c21e37e59272b5cd82ee442b8779b
Jose Nidhin <jose.nidhin@transferto.com>
2019-03-29 18:47:11 +0800

    Refactor code

---
+++ libs/app.go
@@ -0,0 +1,52 @@
+/**
+ * @author Jose Nidhin
+ */
+package libs
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/mux"
+	"go.uber.org/zap"
+
+	"github.com/josnidhin/go-rest-api-example/config"
+	"github.com/josnidhin/go-rest-api-example/routes"
+)
+
+var app *App
+var once sync.Once
+
+type App struct {
+	Config *config.Config
+	Logger *zap.Logger
+	Router *mux.Router
+}
+
+func (a *App) Initilise() {
+	a.Logger = NewLogger(a.Config)
+	a.Router = routes.NewRouter()
+}
+
+func (a *App) Start() {
+	serverAddress := fmt.Sprintf(":%d", a.Config.Server.HTTP.Port)
+
+	err := http.ListenAndServe(serverAddress, a.Router)
+
+	if err != nil {
+		a.Logger.Fatal("Server startup failed", zap.Error(err))
+	}
+}
+
+func AppInstance() *App {
+	once.Do(func() {
+		app = &App{}
+		app.Config = config.Instance()
+
+		app.Initilise()
+		app.Start()
+	})
+
+	return app
+}
--- main.go
+++ main.go
@@ -4,42 +4,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"go.uber.org/zap"
-
-	"github.com/josnidhin/go-rest-api-example/config"
 	"github.com/josnidhin/go-rest-api-example/libs"
-	"github.com/josnidhin/go-rest-api-example/routes"
 )
 
-type App struct {
-	Config *config.Config
-	Logger *zap.Logger
-	Router *mux.Router
-}
-
 func main() {
-	app := &App{}
-	app.Config = config.Instance()
-
-	app.Initilise()
-	app.Start()
-}
-
-func (a *App) Initilise() {
-	a.Logger = libs.NewLogger(a.Config)
-	a.Router = routes.NewRouter()
-}
-
-func (a *App) Start() {
-	serverAddress := fmt.Sprintf(":%d", a.Config.Server.HTTP.Port)
-
-	err := http.ListenAndServe(serverAddress, a.Router)
-
-	if err != nil {
-		a.Logger.Fatal("Server startup failed", zap.Error(err))
-	}
+	libs.AppInstance()
 }
